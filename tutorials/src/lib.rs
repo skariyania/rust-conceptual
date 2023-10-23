70d9881648f8a084964b8e31cb6aa53cb3c8f39a
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-23 22:28:21 +0530

    traits + module arrangements

--- tutorials/src/lib.rs
+++ tutorials/src/lib.rs
@@ -8,6 +8,9 @@ fn another_private_function() {
 }
 
 pub mod front_of_house;
+pub mod news_article;
+pub mod summary;
+pub mod tweet;
 
 pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
