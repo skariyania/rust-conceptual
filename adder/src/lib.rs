5430c62c6c7323f55b58b974e9e2c9cebcf0af2a
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-25 21:53:27 +0530

    test case for rectangle

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -29,4 +29,18 @@ mod tests {
     fn fail_exploration() {
         panic!("this test will fail");
     }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
 }
