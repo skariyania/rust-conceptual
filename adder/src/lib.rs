7af12a8f193115de350f1b7b3b09183923bd17c9
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-25 21:50:51 +0530

    add rectangle struct

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,6 +2,18 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
