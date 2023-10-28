dfb9fedec8aebd4923ec2d6f660bc10ec74b98c3
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-28 21:31:25 +0530

    complete search fn with test pass

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -31,7 +31,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec!["safe, fast, productive."]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 #[cfg(test)]
@@ -42,9 +48,9 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
