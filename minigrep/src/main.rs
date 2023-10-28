ff3af20cac1743b77f905250667ea0eaaa23ed71
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-28 12:19:26 +0530

    fix: variable should init with let

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,11 +2,12 @@ use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    config = parse_config(&args);
-    println!("Searching for '{}'", query);
-    println!("In file '{}'", file_path);
+    let config = parse_config(&args);
+    println!("Searching for '{}'", config.query);
+    println!("In file '{}'", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
@@ -15,6 +16,7 @@ struct Config {
     query: String,
     file_path: String,
 }
+
 fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
     let file_path = args[2].clone();
