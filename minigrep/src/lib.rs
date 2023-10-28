2507abd81baaf93b3c84cac963dd539631eeb362
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-29 00:15:29 +0530

    update main to handle ignore case from env

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,8 +1,9 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
 pub struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -12,8 +13,12 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
     pub fn query(&self) -> &String {
         &self.query
@@ -26,7 +31,14 @@ impl Config {
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
-    for line in search(&config.query(), &contents) {
+
+    let results = if config.ignore_case {
+        search_case_insensetive(&config.query(), &contents)
+    } else {
+        search(&config.query(), &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
     Ok(())
