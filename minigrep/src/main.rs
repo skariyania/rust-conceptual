9f57e5ea6800d5a769fafd6fa0d534cb69f337d7
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-28 13:13:01 +0530

    error handling with Result tpye

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,13 +3,20 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args);
-    println!("Searching for '{}'", config.query);
-    println!("In file '{}'", config.file_path);
+    match config {
+        Ok(config) => {
+            println!("Searching for '{}'", config.query);
+            println!("Searching for '{}'", config.query);
+            let contents = fs::read_to_string(&config.file_path)
+                .expect("Should have been able to read the file");
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+            println!("With text:\n{contents}");
+            println!("In file '{}'", &config.file_path);
+        }
+        Err(message) => {
+            println!("{}", message);
+        }
+    }
 }
 
 struct Config {
@@ -18,13 +25,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
