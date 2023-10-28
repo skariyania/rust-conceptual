5f36ed1ad9e8240fd874116443c1ee9ce3af5399
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-28 23:56:20 +0530

    unit test - case insensetive

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -47,13 +47,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensetive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensetive() {
+        let query = "rust";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(vec!["Rust:", "Trust me."], search(query, contents));
     }
 }
