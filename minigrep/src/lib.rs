8b1b990f48f63ee65453b7ec33337531ad157d66
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-28 21:22:09 +0530

    add search fn add TDD

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -29,3 +29,23 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("With text:\n{contents}");
     Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec!["safe, fast, productive."]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
