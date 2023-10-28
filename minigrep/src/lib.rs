22c20d8de6c9efafaf1f803fbaffa4cba12166a5
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-28 23:34:44 +0530

    add search call in main + update params to reference type

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -24,9 +24,11 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
+pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.file_path)?;
+    for line in search(&config.query(), &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 
