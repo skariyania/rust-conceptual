e2c30d71498ac23b55ea5cb8448d0823d0b8fbb6
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-29 21:05:49 +0530

    learning closures

--- tutorials/src/main.rs
+++ tutorials/src/main.rs
@@ -3,5 +3,6 @@ fn main() {
     // tutorials::media_aggrigator::news_article::run_news_article();
     // tutorials::lifetimes::run_lifetimes_example();
 
-    tutorials::threading_example();
+    // tutorials::threading_example();
+    tutorials::closure_example_trait();
 }
