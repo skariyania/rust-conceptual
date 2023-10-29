1abfd03f6348bf14815ecbec25dcccb0925e77db
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-29 13:23:59 +0530

    update main - giveaway using lib

--- shirt-company/src/main.rs
+++ shirt-company/src/main.rs
@@ -1,3 +1,20 @@
+use shirt_company::{self, Inventory, ShirtColor};
 fn main() {
-    println!("Hello, world!");
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+    let sahil_pref = Some(ShirtColor::Red);
+
+    let giveaway_sahil = store.giveaway(sahil_pref);
+    println!(
+        "The user sahil with preference {:?} gets {:?}",
+        sahil_pref, giveaway_sahil
+    );
+
+    let anonymous_user_preference = None;
+    let giveaway_anonymous = store.giveaway(anonymous_user_preference);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        anonymous_user_preference, giveaway_anonymous
+    );
 }
