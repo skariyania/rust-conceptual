0c3939c74d8f87294ad1dbc2f23c2ed4f5c33a53
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-10-29 09:42:19 +0530

    printling errors to stderr

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,11 +5,11 @@ use minigrep::{run, Config};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     if let Err(e) = run(&config) {
-        println!("Application errored reading file: {e}");
+        eprintln!("Application errored reading file: {e}");
         process::exit(1);
     }
 }
