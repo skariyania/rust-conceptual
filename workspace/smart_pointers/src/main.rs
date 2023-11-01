f457bb483fe52ba345f9d0769ee3e1e3b2e3c723
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-01 22:47:40 +0530

    impl box list to store linked list like data

--- workspace/smart_pointers/src/main.rs
+++ workspace/smart_pointers/src/main.rs
@@ -1,4 +1,14 @@
+use crate::List::{Cons, Nil};
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
 fn main() {
     let b = Box::new(5);
     println!("b= {}", b);
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("value of box list: {:#?}", list);
 }
