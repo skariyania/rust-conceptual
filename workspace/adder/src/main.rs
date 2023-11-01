e48e9a4945a0f30bd32aaf537ab63b3aedfca7a6
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-01 13:31:31 +0530

    add lib in workspace using lib fucntion in bin crate

--- workspace/adder/src/main.rs
+++ workspace/adder/src/main.rs
@@ -1,3 +1,7 @@
+use add_one::{self, add};
 fn main() {
-    println!("Hello, world!");
+    let num1 = 3;
+    let num2 = 5;
+    let add_result = add(num1, num2);
+    println!("calling lib crate add and results of add: {add_result}");
 }
