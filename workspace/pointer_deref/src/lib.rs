5f82188d360902cad219ae59d2502175b3dcb9bc
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-02 21:09:47 +0530

    impl custom type mybox

---
+++ workspace/pointer_deref/src/lib.rs
@@ -0,0 +1,18 @@
+use std::ops::Deref;
+
+#[derive(Debug)]
+pub struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    pub fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
