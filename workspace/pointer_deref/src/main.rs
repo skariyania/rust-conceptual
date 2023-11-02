565ca0c5c614629cfdf88f90e2a3fc0fca451b79
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-02 21:09:47 +0530

    impl custom type mybox

--- workspace/pointer_deref/src/main.rs
+++ workspace/pointer_deref/src/main.rs
@@ -1,7 +1,22 @@
+use pointer_deref::MyBox;
+
 fn main() {
     let x = 5;
     let y = &x;
     println!("values of (x, y) = ({}, {})", x, y);
-    // this is wiered, deref, pointers and reference act as same
+    // this is strange, deref, pointers and reference act as same
     println!("values of (y, *y, &y) = ({}, {}, {})", y, *y, &y);
+    assert_eq!(5, *y);
+    // however below lines wont compile
+    // assert_eq!(5, &y);
+    // assert_eq!(5, y);
+
+    let z = Box::new(x);
+    //deref also works with Box types
+    println!("comparing Box values with deref is possible too");
+    assert_eq!(x, *z);
+
+    let a = MyBox::new(x);
+    println!("value of custom made MyBox: {:?}", a);
+    assert_eq!(x, *a);
 }
