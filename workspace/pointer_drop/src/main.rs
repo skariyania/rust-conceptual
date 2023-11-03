6a8fde410eb22b90f597008d5c58ddb4905af657
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-03 19:59:24 +0530

    impl Drop trait

---
+++ workspace/pointer_drop/src/main.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+struct UserLedger {
+    name: String,
+}
+
+impl Drop for UserLedger {
+    fn drop(&mut self) {
+        println!("Dropping UserLedger with name '{:?}'..", self.name);
+    }
+}
+fn main() {
+    let global_ledger = UserLedger {
+        name: String::from("global"),
+    };
+
+    let local_ledger: UserLedger = UserLedger {
+        name: String::from("local"),
+    };
+    println!("Global ledger created {:?}", global_ledger);
+    println!("Local ledger created {:?}", local_ledger);
+}
