b67250534996e687b03302cf1a3ae61812a8b70b
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-05 22:30:15 +0530

    thread handler to wait for thread to finihs

--- workspace/thread_spawn/src/main.rs
+++ workspace/thread_spawn/src/main.rs
@@ -1,14 +1,22 @@
 use std::{thread, time::Duration};
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("number from spawn thread is: {}", i);
-            thread::sleep(Duration::from_millis(1));
+            thread::sleep(Duration::from_millis(100));
         }
     });
 
     for i in 1..5 {
         println!("number from main thread is: {}", i);
     }
+
+    // use handle join to wait for all the thread to complete processing
+    // note: calling join blocks the thread until all handle thread completes
+    // this also prevents other operations on main
+    handle.join().unwrap();
+
+    // this print statement is blocked by handle.join()
+    println!("main will now exit");
 }
