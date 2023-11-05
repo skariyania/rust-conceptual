a22a86c9bdee3cc8d1d8309f69915d1731b9edca
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-05 16:08:27 +0530

    Rc and RefCell along strong and weak references using tree node

--- workspace/tree_node/src/main.rs
+++ workspace/tree_node/src/main.rs
@@ -1,18 +1,59 @@
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
+#[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 fn main() {
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
+    println!("leaf parent = {:#?}", leaf.parent.borrow().upgrade());
+    println!("\n> creating branch..");
+    println!("\n");
 
-    let branch = Rc::new(Node {
-        value: 5,
-        children: RefCell::new(vec![Rc::clone(&leaf)]),
-    });
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        println!(
+            "inside local scope >> leaf parent = {:?}",
+            leaf.parent.borrow().upgrade()
+        );
+        println!(
+            "inside local scope >> leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf)
+        );
+
+        println!(
+            "inside local scope >> branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch)
+        );
+        println!("local scope ends\n");
+    }
+    println!(
+        "outside local scope >> leaf parent = {:?}",
+        leaf.parent.borrow().upgrade()
+    );
+    println!(
+        "outside local scope >> leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf)
+    );
 }
