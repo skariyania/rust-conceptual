82eeccc65deae56a2c326220e65962c998d0d42d
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-05 08:35:23 +0530

    learning memory leak can happen in RefCell<Rc>

--- workspace/rc_memory_leak/src/main.rs
+++ workspace/rc_memory_leak/src/main.rs
@@ -15,5 +15,24 @@ impl List {
     }
 }
 fn main() {
-    println!("Hello, world!");
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("next item in 'a'= {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after mutating a = {}", Rc::strong_count(&b));
+    println!("a rc count after mutating a = {}", Rc::strong_count(&a));
+
+    // uncomment next lines to see if we have a cyclic reference
+    // this is gonna overflow the stack
+    // println!("a next item = {:?}", a.tail());
 }
