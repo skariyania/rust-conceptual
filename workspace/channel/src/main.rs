9fe25433b310245cd508170c115b63f45f402e8f
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-06 22:24:57 +0530

    communicating in channels via multiple threads

--- workspace/channel/src/main.rs
+++ workspace/channel/src/main.rs
@@ -1,4 +1,4 @@
-use std::{sync::mpsc, thread};
+use std::{sync::mpsc, thread, time::Duration};
 
 fn main() {
     let (tx, rx) = mpsc::channel();
@@ -14,4 +14,36 @@ fn main() {
 
     let received = rx.recv().unwrap();
     println!("Received from channel {}", received);
+
+    // another example
+    let (names_tx, names_rx) = mpsc::channel();
+
+    //closure fn to send names in channel
+    let names_channel = move || {
+        let names = vec![
+            String::from("Sadi"),
+            String::from("George"),
+            String::from("Jerry"),
+            String::from("Lina"),
+        ];
+
+        for val in names {
+            names_tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    };
+
+    // names tx thread
+    let names_tx_thread = thread::spawn(names_channel);
+
+    // names rx thread
+    let names_rx_channel = move || {
+        for received_names in names_rx {
+            println!("Howdy, {received_names}");
+        }
+    };
+    thread::spawn(names_rx_channel);
+
+    //block until names thread send all messages
+    names_tx_thread.join().unwrap();
 }
