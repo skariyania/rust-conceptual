d5253835bfc39971e5b2462a566705389598f5a1
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-07 22:49:48 +0530

    move mutex example to separate fn

--- workspace/mutex/src/main.rs
+++ workspace/mutex/src/main.rs
@@ -1,12 +1,14 @@
 use std::sync::Mutex;
 
 fn main() {
-    let m = Mutex::new(3);
+    mutex_is_simple();
+}
 
+fn mutex_is_simple() {
+    let m = Mutex::new(3);
     {
         let mut numb = m.lock().unwrap();
         *numb = 6;
     }
-
     println!("number was updated with mutex lock: {:?}", m);
 }
