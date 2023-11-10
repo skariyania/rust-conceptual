411b6423c3a38fc0c715eb8a0f83ca5dde8e7752
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-10 21:04:44 +0530

    create workspace: trait_workspace + initial library with trait definition

---
+++ workspace/trait_objects/src/lib.rs
@@ -0,0 +1,14 @@
+pub trait Draw {
+    fn draw(&self);
+}
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
