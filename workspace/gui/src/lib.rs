19a58a095f05b4556b3ce1a6af1c1f2ef2f58d9d
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-10 23:25:08 +0530

    Learning - trait objects (#46)
    * create workspace: trait_workspace
    + initial library with trait definition
    
    * trait_objects: add draw traits  using screen
    
    * rename workspace as gui

---
+++ workspace/gui/src/lib.rs
@@ -0,0 +1,38 @@
+pub trait Draw {
+    fn draw(&self);
+}
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        //code to draw button here
+        println!("@Button > drawing in progress");
+    }
+}
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("@SelectBox > drawing in progress");
+    }
+}
