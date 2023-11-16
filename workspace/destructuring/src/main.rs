fc4f2e096af6b96ed65ed891a35cdbea2729c9b2
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-16 23:51:03 +0530

    destructuring structure values

--- workspace/destructuring/src/main.rs
+++ workspace/destructuring/src/main.rs
@@ -1,3 +1,18 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
 fn main() {
-    println!("Hello, world!");
+    //asserting struct destructuring values working fine
+    struct_destructuring_values();
+}
+
+fn struct_destructuring_values() {
+    // creating point instance and assign reference to 'p'
+    let p = Point { x: 0, y: 1 };
+
+    //assign local variable 'a' and 'b' using point ref 'p'
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(1, b);
 }
