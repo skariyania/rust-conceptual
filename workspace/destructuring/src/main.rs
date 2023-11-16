0fdb8851e8e081c4903e713fd50e21f286ab0419
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-16 23:53:03 +0530

    destruct shorthand

--- workspace/destructuring/src/main.rs
+++ workspace/destructuring/src/main.rs
@@ -5,6 +5,7 @@ struct Point {
 fn main() {
     //asserting struct destructuring values working fine
     struct_destructuring_values();
+    struct_destructuring_values_shorthand();
 }
 
 fn struct_destructuring_values() {
@@ -15,4 +16,14 @@ fn struct_destructuring_values() {
     let Point { x: a, y: b } = p;
     assert_eq!(0, a);
     assert_eq!(1, b);
+}
+
+fn struct_destructuring_values_shorthand() {
+    // creating point instance and assign reference to 'p'
+    let p = Point { x: 0, y: 1 };
+
+    //assign local variable 'a' and 'b' using point ref 'p'
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(1, y);
 }
