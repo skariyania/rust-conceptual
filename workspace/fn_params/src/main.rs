406513a927065bc64d14e5c530e77aef3d7749c9
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-20 18:40:30 +0000

    functions as params

---
+++ workspace/fn_params/src/main.rs
@@ -0,0 +1,12 @@
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+fn main() {
+    let result = do_twice(add_one, 2);
+    // (2+1) + (2+1)
+    println!("add_twise returns: {}", result);
+}
