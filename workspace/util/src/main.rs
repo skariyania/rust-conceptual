9ae8c84543302842900db8773d23c096112aa37a
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-21 17:41:47 +0000

    map with enums

--- workspace/util/src/main.rs
+++ workspace/util/src/main.rs
@@ -9,4 +9,16 @@ fn main() {
     // using function
     let list_of_strings_f: Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
     println!("List of strings using function: {:?}", list_of_strings_f);
+
+    //Note: closure and function as parameter both compiles to same code
+
+    //we can also use enums in map
+    #[derive(Debug)]
+    enum Status {
+        Value(u32),
+        Stop,
+    }
+
+    let list_of_statuses: Vec<Status> = (0..5).map(Status::Value).collect();
+    println!("collected enum values {:?}", list_of_statuses);
 }
