f039f4e155fb1fb601dec3214778256c887aa10a
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-22 18:09:58 +0000

    handling http connection

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,10 +1,22 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Request: {:#?}", http_request);
+}
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("starting server :8080");
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("connection established");
-        println!("streaming.. {:?}", stream);
+        handle_connection(stream);
     }
 }
