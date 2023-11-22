19a2fa2600dbb64cb9abb0416a572bac8b1a48e5
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-22 17:53:08 +0000

    create streaming server listening on port 8080

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+use std::net::TcpListener;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("connection established");
+        println!("streaming.. {:?}", stream);
+    }
+}
