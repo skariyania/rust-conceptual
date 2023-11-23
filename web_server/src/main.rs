f29b88b8431ac55652b87e2667e845cc63350678
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-23 17:57:24 +0000

    Add handling for /sleep endpoint

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 fn request_logger(buf_reader: BufReader<&mut TcpStream>) {
     let http_request: Vec<_> = buf_reader
@@ -18,10 +20,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1 200 OK", "404.json")
+    let (status_line, file_name) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 200 OK", "404.json"),
     };
     let file_path = String::from("web_server/static/");
     let contents = fs::read_to_string(file_path + file_name).unwrap();
