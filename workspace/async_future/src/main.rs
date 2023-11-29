7c573b96d37f79470d704c762544fec16d8e568d
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-11-29 23:50:48 +0530

    creating async fn using async in sync with block_on

--- workspace/async_future/src/main.rs
+++ workspace/async_future/src/main.rs
@@ -4,9 +4,34 @@ async fn hello_world() {
     println!("hello_world");
 }
 
+async fn learn(skill: String) {
+    println!("learning {}", skill)
+}
+
+async fn sing() {
+    learn(String::from("how to sing")).await;
+    println!("I can sing")
+}
+
+async fn dance() {
+    println!("I can dance")
+}
+
+async fn async_main() {
+    let learn = learn(String::from("programming"));
+    let sing = sing();
+    let dance = dance();
+
+    // as specified in book, join! macro is like await,
+    // but it can wait for multiple futures
+    futures::join!(sing, dance, learn);
+}
+
 fn main() {
-    let future = hello_world();
     // remember futures are lazy executor,
     // above line will not execute anything yet
-    block_on(future); // run future in current thread
+    let hello_world_future = hello_world();
+
+    block_on(async_main());
+    block_on(hello_world_future); // run future in current thread
 }
