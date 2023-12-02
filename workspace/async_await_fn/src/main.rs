348612e97b0cb9216f90c87d2fb339f8373383b9
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-12-03 00:36:05 +0530

    notes for reference

--- workspace/async_await_fn/src/main.rs
+++ workspace/async_await_fn/src/main.rs
@@ -29,3 +29,9 @@ async fn fun(x: &u8) -> u8 {
 fn fun_expanded_internal<'a>(x: &'a u8) -> impl Future<Output = u8> + 'a {
     async move { *x }
 }
+
+// meaning, unlike traditional functions,
+// async function which takes references or other non 'static arguments return a future
+// which is bounded by lifetime parameters
+// If we store the future or send it over to another thread, it can create issue without 'static
+// we may see and error `does not live long enough`
