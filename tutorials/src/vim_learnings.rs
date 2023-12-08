2da7979cd17da8838eb564d539b86ed791c810a8
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-12-08 21:30:44 +0530

    cleanup and typo fix (#88)

--- tutorials/src/vim_learnings.rs
+++ tutorials/src/vim_learnings.rs
@@ -1,9 +1,9 @@
 //hello I am new comment
 fn main() {
     for n in () {
-        //some more comments
-        //dom something rocha
-        //some more connets
-        // abocec was wrong lets ccorrect
+        // some more comments
+        // done something rocha
+        // some more comments
+        // above was wrong lets correct
     }
 }
