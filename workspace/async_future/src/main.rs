05b2084916246513f2cc8930bff905ed344c31d5
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-12-09 23:23:13 +0530

    async joins using book and music (#89)

--- workspace/async_future/src/main.rs
+++ workspace/async_future/src/main.rs
@@ -1,4 +1,4 @@
-use futures::executor::block_on;
+use futures::{executor::block_on, try_join, TryFutureExt};
 
 async fn hello_world() {
     println!("hello_world");
@@ -27,6 +27,35 @@ async fn async_main() {
     futures::join!(sing, dance, learn);
 }
 
+#[derive(Debug)]
+struct Book {
+    title: String,
+    author: String,
+}
+#[derive(Debug)]
+struct Music {
+    genre: String,
+}
+
+async fn get_book(title: String, author: String) -> Result<Book, String> {
+    println!("searching book.. {} {}", title, author);
+    Ok(Book { author, title })
+}
+
+async fn get_music(genre: String) -> Result<Music, String> {
+    println!("searching music.. {}", genre);
+    Ok(Music { genre })
+}
+
+async fn get_book_and_music() -> Result<(Book, Music), String> {
+    let book_fut = get_book(
+        "Sahil Kariyania".to_string(),
+        "Rust Conceptual Learnings".to_string(),
+    )
+    .map_err(|_| "Unable to get book".to_string());
+    let music_fut = get_music("Verse".to_string()).map_err(|_| "Unable to get Music".to_string());
+    try_join!(book_fut, music_fut)
+}
 fn main() {
     // remember futures are lazy executor,
     // above line will not execute anything yet
@@ -34,4 +63,10 @@ fn main() {
 
     block_on(async_main());
     block_on(hello_world_future); // run future in current thread
+
+    // this is an example to use try_join
+    // let details = get_book_and_music();
+    // details
+    let (book_details, music_details) = block_on(get_book_and_music()).unwrap();
+    println!("Book={:#?} and music={:#?}", book_details, music_details);
 }
