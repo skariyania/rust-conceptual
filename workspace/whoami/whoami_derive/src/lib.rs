b472ce3249e33773ccfe8d89154ab5e14c4142f2
Sahil Kariyania <sahil.kariyania@gmail.com>
2023-12-15 22:56:27 +0530

    remove unnecessary (#97)
    typo correction

--- workspace/whoami/whoami_derive/src/lib.rs
+++ workspace/whoami/whoami_derive/src/lib.rs
@@ -1,6 +1,5 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn;
 
 fn impl_whoami_macro(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
@@ -19,6 +18,6 @@ pub fn whoami_macro_derive(input: TokenStream) -> TokenStream {
     // construct representation of rust code as abstract syntax tree
     let ast = syn::parse(input).unwrap();
 
-    //build trait implementaion
+    //build trait implementation
     impl_whoami_macro(&ast)
 }
