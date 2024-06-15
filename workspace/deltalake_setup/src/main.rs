dc4a4bff0aebca78fba6d155a34c74da34cdfdd6
Sahil Kariyania <sahil.kariyania@gmail.com>
2024-06-15 20:23:18 +0530

    upgrade deltalake

--- workspace/deltalake_setup/src/main.rs
+++ workspace/deltalake_setup/src/main.rs
@@ -3,7 +3,7 @@ use deltalake::{open_table, DeltaTableError};
 #[tokio::main]
 async fn main() -> Result<(), DeltaTableError> {
     let table = open_table("./workspace/deltalake_setup/data/delta/").await?;
-    let files = table.get_files();
-    println!("{:?}", files);
+    let files_count = table.get_files_count();
+    println!("files count in delta lake {:?}", files_count);
     Ok(())
 }
