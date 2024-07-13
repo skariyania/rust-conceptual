b25d790bb8001149a8af55e6e11db6fd9c1cd2bb
Sahil Kariyania <sahil.kariyania@gmail.com>
2024-07-13 18:07:30 +0530

    implemented get and set in server

---
+++ example/cachers-client/src/main.rs
@@ -0,0 +1,16 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:63790").await?;
+
+    // Set the key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+
+    // get key hello from server
+    let result = client.get("hello").await?;
+
+    println!("got value from server; result={:?}", result);
+
+    Ok(())
+}
--- example/cachers/src/main.rs
+++ example/cachers/src/main.rs
@@ -1,4 +1,9 @@
-use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+
+use mini_redis::{
+    Command::{self, Get, Set},
+    Connection, Frame,
+};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -7,17 +12,33 @@ async fn main() {
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 
 async fn process(socket: TcpStream) {
+    let mut db: HashMap<String, Vec<u8>> = HashMap::new();
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
 
-        let response = Frame::Error("unimplemented".to_string());
+            cmd => panic!("Unimplemented command {:?}", cmd),
+        };
+
         connection.write_frame(&response).await.unwrap();
     }
 }
