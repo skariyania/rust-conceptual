4ad3207148850c1ed6512a1018872a688d4965cd
Sahil Kariyania <sahil.kariyania@gmail.com>
2024-07-13 21:06:29 +0530

    enabled concurrency - server: spawn with shared db references - client: using message passing techniques

--- example/cachers-client/src/main.rs
+++ example/cachers-client/src/main.rs
@@ -1,16 +1,51 @@
-use mini_redis::{client, Result};
+use bytes::Bytes;
 
+use mini_redis::{client, Result};
+use tokio::sync::mpsc;
+
+#[derive(Debug)]
+enum Command {
+    Get { key: String },
+    Set { key: String, value: Bytes },
+}
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:63790").await?;
+    let (tx, mut rx) = mpsc::channel(32);
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:63790").await.unwrap();
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
+            match cmd {
+                Get { key } => {
+                    println!("received command= Get key = {:?}", key);
+                    client.get(&key).await.unwrap();
+                }
+                Set { key, value } => {
+                    println!("received command= Set key = {:?}, val = {:?}", key, val);
+                    client.set(&key, value).await.unwrap();
+                }
+            }
+        }
+    });
 
-    // Set the key "hello" with value "world"
-    client.set("hello", "world".into()).await?;
+    let tx2 = tx.clone();
+    let t1 = tokio::spawn(async move {
+        let cmd = Command::Get {
+            key: "foo".to_string(),
+        };
+        tx.send(cmd).await.unwrap();
+    });
+    let t2 = tokio::spawn(async move {
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            value: "bar".into(),
+        };
+        tx2.send(cmd).await.unwrap();
+    });
 
-    // get key hello from server
-    let result = client.get("hello").await?;
-
-    println!("got value from server; result={:?}", result);
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
 
     Ok(())
 }
--- example/cachers/src/main.rs
+++ example/cachers/src/main.rs
@@ -1,5 +1,9 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
+use bytes::Bytes;
 use mini_redis::{
     Command::{self, Get, Set},
     Connection, Frame,
@@ -9,30 +13,35 @@ use tokio::net::{TcpListener, TcpStream};
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:63790").await.unwrap();
+    println!("listening ..");
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        // clone the handle to hash map db
+        let db = db.clone();
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
-    let mut db: HashMap<String, Vec<u8>> = HashMap::new();
+async fn process(socket: TcpStream, db: Arc<Mutex<HashMap<String, Bytes>>>) {
     let mut connection = Connection::new(socket);
 
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
             }
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
 
