81bdc5472d82eba64861f03a93583ff82e9d05f3
Sahil Kariyania <sahil.kariyania@gmail.com>
2024-07-17 22:23:02 +0530

    Redis like cache server implementation - tokio crate learnings (#145)
    * redis poc server
    
    * implemented get and set in server
    
    * added bytes cargo
    
    * enabled concurrency
    - server: spawn with shared db references
    - client: using message passing techniques
    
    * typo
    
    * spellchecker
    
    * receiving responses from message passing queries

---
+++ example/cachers/src/main.rs
@@ -0,0 +1,56 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use bytes::Bytes;
+use mini_redis::{
+    Command::{self, Get, Set},
+    Connection, Frame,
+};
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:63790").await.unwrap();
+    println!("listening ..");
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        // clone the handle to hash map db
+        let db = db.clone();
+        tokio::spawn(async move {
+            process(socket, db).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream, db: Arc<Mutex<HashMap<String, Bytes>>>) {
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Get(cmd) => {
+                let db = db.lock().unwrap();
+                if let Some(value) = db.get(cmd.key()) {
+                    println!("processed GET {:?}", cmd);
+                    Frame::Bulk(value.clone())
+                } else {
+                    println!("processed GET {:?}", cmd);
+                    Frame::Null
+                }
+            }
+            Set(cmd) => {
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                println!("processed SET {:?}", cmd);
+                Frame::Simple("OK".to_string())
+            }
+
+            cmd => panic!("Unimplemented command {:?}", cmd),
+        };
+
+        connection.write_frame(&response).await.unwrap();
+    }
+}
